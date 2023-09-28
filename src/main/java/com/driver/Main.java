2b0cf6c7b1ea6ddb99dc2818a75ba8901c68fe07
Vinay C N <vinaycngowda@gmail.com>
2023-09-28 14:45:19 +0530

    runTimePoly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+ public static void main(String[] args){
+     Product p = new Product();
+     p.Product(5,10);
+     p.Product(5,10,15);
+     p.Product(1.0,2.0);
 
+
+ }
+    public static class Product{
+        public int Product(int x , int y){
+            return x+y;
+        }
+
+        public int Product(int x , int y ,int z){
+            return x+y+z;
+        }
+
+        public double Product(double x ,double y ){
+            return x*y;
+        }
+    }
 }
